7d07f6576a138f6586a8df6dabb860b6ef982a0c
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-02-28 17:34:08 +0530

    day 82 of 1000 days of code

---
+++ day82/day82.java
@@ -0,0 +1,78 @@
+public class day82 {
+
+    public static void main(String[] args) {
+        
+        /*
+        Operators in java
+        Arithmetic, Relational operator, Logical operator, Bitwisae operator, Assignment operator
+
+        Relational operator:
+        ==
+        equal to - equal to==> it helps to operates as the relation between the operands 
+        if the condition gets satisfy it will states as trues and if not it is false.
+        
+        != 
+        not equal to ==> it always the opposite of the equal to(==) 
+        if the condition is true it will states as false and if true it will states as true.
+
+        > 
+        greater than it shows which is greater than the other operand
+
+        <
+        less than it shows which is greater than the operands
+
+        <=
+        less than or equals to
+
+        >=
+        Greater than or equal too
+
+
+                Logical operator
+
+                (&&)Logical and operator:
+                when there is only both the value is true in the logic then only the loghisc will be true otherwise it will be false in every condition.
+
+                Statement1(true) && Statement2(true) ==> True
+                
+                
+                (||) Logical OR operator:
+                when the value from both the statement will be true then the whole condition will be true but
+                if two of the statements are false then the whole condition is false.
+
+                Statement1(false) || Statement2(False) ==> False
+
+                (!) logical Not Operator :
+                true to false
+                and false to true.
+
+                for example:
+                Sout( !(5>0))
+
+                output: False
+
+
+                        #Assignment operator:
+                        (=) where the value get assigned from one to another through this sign "="
+                        int a = b
+                        if a=10 and b=5 then a become 5 after assigning.
+
+                        (+) where a can be a = a+10 in competitive we can write as a+ = 10;
+                        a=20
+
+                        (-) where a can be a- = 10;
+
+                        (*) where b can be b* = 5; b=25
+
+                        (/) where b can be b/ = 5; 0 remainder is 0.
+                        
+
+
+
+        
+        */ 
+
+        System.out.println( (3>2) && (1>0));
+
+    }
+}
---
+++ day82/day82condition.java
@@ -0,0 +1,21 @@
+public class day82condition {
+    public static void main(String[] args) {
+        /*
+        when we do something with some intention to get the desired result is known as the condition.
+
+        if else, else if, ternary operator, switch
+        */ 
+
+        //if - else condition in which it first check the condition and then it prints
+        
+        // int age =18; or variable declaration
+
+        // if (condition) {
+            
+        // } 
+        
+        // else {
+            
+        // }
+    }
+}
