fb19e8ff7ea7b099c1744df3901046a335a188da
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-03-01 23:34:55 +0530

    day 84 of 1000 days of code

---
+++ day84/day84.java
@@ -0,0 +1,15 @@
+package day84;
+
+public class day84 {
+    public static void main(String[] args) {
+        int n;
+        n = 5;
+        if (n % 2 == 1) {
+            System.out.println("Odd");
+        } 
+        
+        else {
+            System.out.println("Even");
+        }     // correct the closing bracket
+    }
+}
