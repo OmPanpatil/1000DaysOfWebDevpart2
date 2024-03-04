3bdce960e4c68ffaec8515b7cbf2e9b1e0ed7355
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-03-04 20:08:16 +0530

    day 87 of 1000 days of code

---
+++ day87/day87.java
@@ -0,0 +1,41 @@
+package day87;
+
+import java.util.Scanner;
+
+
+public class day87 {
+    public static void main(String[] args) {
+        /*
+        to  check if a number is prime or not
+        */ 
+
+        Scanner Sc = new Scanner(System.in);
+        //n= under root n*n
+        
+        System.out.print("Enter the number to check if it is prime or not : ");
+        int number = Sc.nextInt();
+        
+        if (number == 2) {
+            System.out.println("It is prime.");
+        } else {
+                boolean isprime = true;
+                for(int i = 1; i<=Math.sqrt(number); i++){
+                    if (number%i == 0) {
+                    isprime=false;
+                    }
+                }
+    
+                if (isprime==true) {
+                    System.out.println("It is prime number");
+                } else 
+                {
+                    System.out.println("number is not prime"); 
+                }
+
+
+            
+            }
+
+
+    }
+}
