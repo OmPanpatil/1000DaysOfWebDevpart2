aff193dfea0730da43d9a6e87de92bfd65097f46
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-03-06 19:40:40 +0530

    day 89 of 1000 days of code

---
+++ day89/day89.java
@@ -0,0 +1,20 @@
+package day89;
+
+class day89 {
+
+    public static void printhello(){
+        System.out.println("Hello world, i will be starting with competitive coding!");
+        System.out.println("Hello world, i will be starting with competitive coding!");
+        return;
+    }
+
+    public static void main(String[] args) {
+        /*
+        returnType name (){
+            body;
+            return statement;
+        }
+        */ 
+        printhello(); //function call
+    }
+}
