817cd46ea3edc5fd590b7b7029e0b7c0fe8f027b
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-03-15 23:16:21 +0530

    day 98 of 1000 days of code

---
+++ day98/day98.java
@@ -0,0 +1,11 @@
+package day98;
+
+public class day98 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        float b = a;
+        System.out.println(b);
+        
+    }
+}
