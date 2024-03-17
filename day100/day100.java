c5c4d8a8bbbe8a8cb63fa999028b7f86a71efcb2
Om Panpatil <139043319+OmPanpatil@users.noreply.github.com>
2024-03-17 19:03:22 +0530

    mark the day 100 of 1000 days of code with java problem from coding ninjas

---
+++ day100/day100.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class day100 {
+
+ 
+
+public static void main(String args[]) {
+
+ 
+
+// Write code here
+
+//take input
+
+try (Scanner sc = new Scanner(System.in)) {
+    char ch = sc.next().charAt(0);
+    
+     
+    
+    //check the conditions and print the values
+    
+    if(Character.isUpperCase(ch))
+    
+    {
+    
+    System.out.println("1");
+    
+    }
+    
+    else if(Character.isLowerCase(ch))
+    
+    {
+    
+    System.out.println("0");
+    
+    }
+    
+    else
+    
+    {
+    
+    System.out.println("-1");
+    
+    }
+}
+
+ 
+
+}
+
+}
